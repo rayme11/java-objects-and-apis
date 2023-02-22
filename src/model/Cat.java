4ebf097951155af57f7e4b609294d39269bfc328
Ray Maldonado <vivachihuahua2004@gmail.com>
2023-02-22 16:02:26 -0600

    Chapter 2  Final

---
+++ src/model/Cat.java
@@ -0,0 +1,13 @@
+package model;
+
+public class Cat extends Animal {
+
+    @Override
+    public void makeSound() {
+        System.out.println("meow");
+    }
+
+    public void scratch(){
+        System.out.println("I am a cat, I scratch things...");
+    }
+}
---
+++ src/model/Form.java
@@ -0,0 +1,10 @@
+package model;
+
+public abstract class Form {
+
+    abstract double CalculateArea();
+
+    public void print(){
+        System.out.println("I am a form");
+    }
+}
